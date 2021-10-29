162aa78e56314fb385b3f6748913779dfb417cc3
Frank-Proulx <frankproulx7@gmail.com>
2021-10-29 10:10:02 -0700

    add the numberChecker function having successfully tested it

--- js/scripts.js
+++ js/scripts.js
@@ -13,3 +13,23 @@ function numberSplitter(number) {
   });
   return newSplitter;
 }
+
+function numberChecker(array) {
+  for (i = 0; i < array.length; i++) {
+    if (array[i] === 3) {
+      return "Won't you be my neighbor?";
+    }
+  }
+  for (j = 0; j < array.length; j++) {
+    if (array[j] === 2) {
+      return "Boop!";
+    }
+  }
+  for (k = 0; k < array.length; k++) {
+    if (array[k] === 1) {
+      return "Beep!";
+    }
+  }
+  return parseInt(array.join(''));
+}
+
